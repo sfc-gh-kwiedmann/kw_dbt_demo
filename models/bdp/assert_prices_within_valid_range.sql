0c0570572e73ff742ff60011dfe93c7f8b95d55d
KWIEDMANN <katharina.wiedmann@snowflake.com>
2026-08-12 17:05:44 +0000

    Adding back in folder

--- models/bdp/assert_prices_within_valid_range.sql
+++ models/bdp/assert_prices_within_valid_range.sql
@@ -1,6 +1,6 @@
 -- Test that all product prices fall within a valid range (0 to 10000).
 -- Co-authored with CoCo
-{{ custom_tests.test_column_in_range(
+{{ test_column_in_range(
     model=ref('products_enriched'),
     column_name='price',
     min_value=0,
